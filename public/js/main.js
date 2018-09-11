d5cbd88be3561700bfa8d2b4b4cc843841d2a03a
Dacrol <jonas.ahrens@gmail.com>
2018-09-11 09:15:34 +0200

    Make sidebar use flexbox

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
-$.getJSON("/json/recipies.json", function (data) { 
-    data.forEach(recipe => $('#recipe-list')
-    .append(`<div class="col-4 mb-5">
+$.getJSON('/json/recipies.json', function(data) {
+  data.forEach(recipe =>
+    $('#recipe-list').append(`<div class="col-4 mb-5">
     <div class="card h-100 mb-4 shadow-sm">
-      <img class="card-img-top" alt="" style="height: 225px; width: 100%; display: block;" src="${recipe.image}">
+      <img class="card-img-top recipe-thumbnail" alt="" src="${recipe.image}">
       <div class="card-body d-flex flex-column justify-content-between">
         <p class="card-text">${recipe.summary}</p>
         <div class="d-flex justify-content-between align-items-center">
@@ -11,6 +11,6 @@ $.getJSON("/json/recipies.json", function (data) {
         </div>
       </div>
     </div>
-  </div>`))
-  }
-);
+  </div>`)
+  )
+})
