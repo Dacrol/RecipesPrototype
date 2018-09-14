f412dc11275d6ec13ccf646199b96c4e1f9a8af4
Dacrol <jonas.ahrens@gmail.com>
2018-09-14 15:51:49 +0200

    Prettify all code

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
-$.getJSON('/json/recipies.json', function (data) {
+$.getJSON('/json/recipies.json', function(data) {
   data.forEach(recipe =>
-    $('#recipe-list').append(`<div class="col-12 col-md-6 col-lg-4 mb-5"><a href="recdetails.html">
+    $('#recipe-list')
+      .append(`<div class="col-12 col-md-6 col-lg-4 mb-5"><a href="recdetails.html">
     <div id="${recipe.dish}" class="card h-100 mb-4 shadow-sm">
-      <img class="card-img-top recipe-thumbnail" alt="${recipe.dish}" src="${recipe.image}">
+      <img class="card-img-top recipe-thumbnail" alt="${recipe.dish}" src="${
+      recipe.image
+    }">
       <div class="card-body d-flex flex-column justify-content-between">
         <p class="card-text">${recipe.summary}</p>
         <div class="d-flex justify-content-between align-items-center">
@@ -18,27 +21,27 @@ $.getJSON('/json/recipies.json', function (data) {
 // for new.html
 
 // Lägg till hela receptet
-$(document).on('click', '.btn-add-recipe', function(){
-  $('.add-success').toggleClass('d-none');
-});
+$(document).on('click', '.btn-add-recipe', function() {
+  $('.add-success').toggleClass('d-none')
+})
 
 // Lägg till ny instruktion
-$(document).on('click', '.btn-add-instruction', function(){
-  if($('.add-instruction').val()){
-  $('.added-instructions').append(`
+$(document).on('click', '.btn-add-instruction', function() {
+  if ($('.add-instruction').val()) {
+    $('.added-instructions').append(`
     <li class="mt-1">${$('.add-instruction').val()}</li>
-  `);
+  `)
   }
-  $('.add-instruction').val('');
-});
+  $('.add-instruction').val('')
+})
 
-$(document).on('click', '.btn-add-ingredient', function(){
-  
-  if(($('.add-ingredient').val()) && ($('.add-volume').val())){
-  $('.added-ingredient').append(`
-    <li class="mt-1">${$('.add-ingredient').val()} ${$('.add-volume').val()}</li>
-  `);
-  $('.add-ingredient, .add-volume').val('');
+$(document).on('click', '.btn-add-ingredient', function() {
+  if ($('.add-ingredient').val() && $('.add-volume').val()) {
+    $('.added-ingredient').append(`
+    <li class="mt-1">${$('.add-ingredient').val()} ${$(
+      '.add-volume'
+    ).val()}</li>
+  `)
+    $('.add-ingredient, .add-volume').val('')
   }
-});
-
+})
