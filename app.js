bb194623723c31c777664443ea3b1dd183c448e7
Dacrol <jonas.ahrens@gmail.com>
2018-09-15 18:18:16 +0200

    Add base server setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+var exphbs = require('express-handlebars')
+
+var app = express()
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+/* app.get('/', function(req, res) {
+  res.render('home')
+}) */
+
+app.use(express.static('public'));
+app.listen(3000, () => {console.log('Server open on port 3000')})
