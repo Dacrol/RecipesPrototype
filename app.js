098b7c837cd7962fb4f384a09871864208403b6d
Dacrol <jonas.ahrens@gmail.com>
2018-09-17 17:05:00 +0200

    Cleanup code

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require('express')
-var exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars')
 
-var app = express()
+const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-/* app.get('/', function(req, res) {
-  res.render('home')
-}) */
-
-
-app.engine('html', exphbs({
-  extname: 'html',
-  defaultLayout: 'main'
-}))
+app.engine(
+  'html',
+  exphbs({
+    extname: 'html',
+    defaultLayout: 'main'
+  })
+)
 app.set('view engine', 'html')
 
-app.get('/', function (req, res) {
-  res.render('home', {layout: 'main-sidebar'})
+app.get('/', function(req, res) {
+  res.render('home', { layout: 'main-sidebar' })
 })
 
-app.get('/recipe/:id', function (req, res) {
+app.get('/recipe/:id', function(req, res) {
   res.render('recipe')
 })
 
-app.get('/new', function (req, res) {
+app.get('/new', function(req, res) {
   res.render('new')
 })
 
-app.use(express.static('public'));
-app.listen(3000, () => {console.log('Server open on port 3000')})
+app.use(express.static('public'))
+app.listen(3000, () => {
+  console.log('Server open on port 3000')
+})
