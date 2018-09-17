49aa63b9ecb715f49d88d07546686a7474bd729a
Dacrol <jonas.ahrens@gmail.com>
2018-09-17 16:46:08 +0200

    Add routes for new and recipe views

--- app.js
+++ app.js
@@ -21,5 +21,13 @@ app.get('/', function (req, res) {
   res.render('home', {layout: 'main-sidebar'})
 })
 
+app.get('/recipe/:id', function (req, res) {
+  res.render('recipe')
+})
+
+app.get('/new', function (req, res) {
+  res.render('new')
+})
+
 app.use(express.static('public'));
 app.listen(3000, () => {console.log('Server open on port 3000')})
