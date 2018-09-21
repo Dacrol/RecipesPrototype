fba61446c42a6873b3e4459e8be189ab8193ad9c
Johan_M <jmiklo@outlook.com>
2018-09-21 13:28:57 +0200

    removed console.logs

--- public/js/shoppinglist.js
+++ public/js/shoppinglist.js
@@ -8,12 +8,7 @@ function renderShoppinglist() {
       html += `<tr class="d-flex align-items-center"><td class=""><i class="far fa-square check"></i></td><td>${ingredient.amount}${ingredient.unit} ${ingredient.name}</td></tr>\n`
       ingredientlist.push(ingredient)
     });
-
-    console.log(html)
     $('#ingredient-list').append(html);
-
-
-    console.log(ingredientlist);
   } catch (error) {
     console.log('Shoppinglistan är tom');
     console.error(error)
