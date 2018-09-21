7aa3f3511d5eff72c5ff7993cfe4ac54f892f1ab
Dacrol <jonas.ahrens@gmail.com>
2018-09-21 14:46:12 +0200

    Add RecipesFilter class

--- public/js/recipesFilter.js
+++ public/js/recipesFilter.js
@@ -1,23 +1,72 @@
-db.collection('Recipes')
-  .get()
-  .then(recipes => {
-    const existingIngredients = new Set()
-    recipes.forEach(recipe => {
-      recipe.data().ingredients.forEach(ingredient => {
-        existingIngredients.add(capitalizeFirstLetter(ingredient.name))
+class RecipesFilter {
+  constructor() {
+    this.filterBy = []
+  }
+
+
+ 
+
+  renderAllRecipes() {
+    db.collection('Recipes')
+      .get()
+      .then(querySnapshot => {
+        Promise.all(querySnapshot.docs.map(doc => doc.data())).then(
+          this.renderRecipes
+        )
       })
+  }
+
+  renderRecipes(recipes) {
+    $('#recipe-list').empty()
+    recipes.forEach(recipe => {
+      $('#recipe-list')
+        .append(`<div class="col-12 col-md-6 col-xl-4 mb-5"><a href="/recipe/${formatUrl(
+        recipe.dish
+      )}">
+  <div id="${recipe.dish}" class="card h-100 mb-4 shadow-sm">
+    <img class="card-img-top recipe-thumbnail" alt="${recipe.dish}" src="${
+        recipe.image
+      }">
+    <div class="card-body d-flex flex-column justify-content-between">
+      <p class="card-text">${recipe.summary}</p>
+      <div class="d-flex justify-content-between align-items-center">
+        <i class="far fa-clock fa-1point5"></i>
+        <small class="text-muted">${recipe.time}</small>
+      </div>
+    </div>
+  </div>
+  </div></a>`)
     })
-    Array.from(existingIngredients).sort().forEach(ingredient => {
-      $('#ingredients-list').append(`<div class="form-check">
+  }
+}
+
+const recipesFilter = new RecipesFilter()
+recipesFilter.renderAllRecipes()
+
+;(function addIngredientsToSidebar() {
+  db.collection('Recipes')
+    .get()
+    .then(recipes => {
+      const existingIngredients = new Set()
+      recipes.forEach(recipe => {
+        recipe.data().ingredients.forEach(ingredient => {
+          existingIngredients.add((ingredient.name))
+        })
+      })
+      Array.from(existingIngredients)
+        .sort()
+        .forEach(ingredient => {
+          $('#ingredients-list').append(`<div class="form-check">
   <input class="form-check-input" type="checkbox" value="" data-ingredient="${ingredient}" id="${formatUrl(
-    ingredient
-  )}">
+            ingredient
+          )}">
   <label class="form-check-label" for="${formatUrl(ingredient)}">
-    ${ingredient}
+    ${capitalizeFirstLetter(ingredient)}
   </label>
 </div>`)
+        })
     })
-  })
+})()
 
 // Make sure scrollbar is hidden even if it's not 17px wide
 $('#sidebar').on('shown.bs.collapse', function() {
@@ -27,36 +76,6 @@ $('#sidebar').on('shown.bs.collapse', function() {
       417 - (collapsible.offsetWidth - collapsible.clientWidth) + 'px'
   }
 })
-
-db.collection('Recipes')
-  .get()
-  .then(querySnapshot => {
-    Promise.all(querySnapshot.docs.map(doc => doc.data())).then(renderRecipes)
-  })
-
-
-function renderRecipes(recipes) {
-  $('#recipe-list').empty()
-  recipes.forEach(recipe => {
-    $('#recipe-list')
-      .append(`<div class="col-12 col-md-6 col-xl-4 mb-5"><a href="/recipe/${formatUrl(
-      recipe.dish
-    )}">
-<div id="${recipe.dish}" class="card h-100 mb-4 shadow-sm">
-  <img class="card-img-top recipe-thumbnail" alt="${recipe.dish}" src="${
-      recipe.image
-    }">
-  <div class="card-body d-flex flex-column justify-content-between">
-    <p class="card-text">${recipe.summary}</p>
-    <div class="d-flex justify-content-between align-items-center">
-      <i class="far fa-clock fa-1point5"></i>
-      <small class="text-muted">${recipe.time}</small>
-    </div>
-  </div>
-</div>
-</div></a>`)
-  })
-}
 
 function capitalizeFirstLetter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
