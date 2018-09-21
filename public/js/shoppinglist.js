6daa199d9531b1103bf374db30f0d11e8d8082a0
Johan_M <jmiklo@outlook.com>
2018-09-21 13:05:16 +0200

    Finalised printout function

--- public/js/shoppinglist.js
+++ public/js/shoppinglist.js
@@ -1,13 +1,35 @@
+let ingredientlist = [];
 renderShoppinglist();
 
 function renderShoppinglist() {
   html = '';
   try {
-    JSON.parse(localStorage.getItem('shoppinglist')).forEach(ingredient => html += `<li>${ingredient.amount} ${ingredient.unit} ${ingredient.name}</li>\n`);
-  console.log(html)
-  $('#shopping-list').append(html);
+    JSON.parse(localStorage.getItem('shoppinglist')).forEach(ingredient => {
+      html += `<tr class="d-flex align-items-center"><td class=""><i class="far fa-square check"></i></td><td>${ingredient.amount}${ingredient.unit} ${ingredient.name}</td></tr>\n`
+      ingredientlist.push(ingredient)
+    });
+
+    console.log(html)
+    $('#ingredient-list').append(html);
+
+
+    console.log(ingredientlist);
   } catch (error) {
     console.log('Shoppinglistan är tom');
+    console.error(error)
   }
-  
+
 }
+
+
+function printData() {
+  $('#printer').hide();
+  window.print();
+  setTimeout(() => {
+    $('#printer').show();
+  }, 200);
+}
+
+$('#printer').on('click', function () {
+  printData();
+})
