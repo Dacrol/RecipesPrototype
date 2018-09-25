864bbd5a585820a81e444f30fc022b13e7b673cf
Johan_M <jmiklo@outlook.com>
2018-09-25 11:44:09 +0200

    added portion based recalculation for shoppinglist

--- public/js/shoppinglist.js
+++ public/js/shoppinglist.js
@@ -5,7 +5,7 @@ function renderShoppinglist() {
   html = '';
   try {
     JSON.parse(localStorage.getItem('shoppinglist')).forEach(ingredient => {
-      html += `<tr class="d-flex align-items-center"><td class=""><i class="far fa-square check"></i></td><td>${ingredient.amount}${ingredient.unit} ${ingredient.name}</td></tr>\n`
+      html += `<tr class="d-flex align-items-center"><td class=""><i class="far fa-square check"></i></td><td>${ingredient.amount || ''}${ingredient.unit} ${ingredient.name}</td></tr>\n`
       ingredientlist.push(ingredient)
     });
     $('#ingredient-list').append(html);
