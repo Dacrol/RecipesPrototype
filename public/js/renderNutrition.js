ba68be0f172f11cba23cb6fdd72764b9415b82a0
Dacrol <jonas.ahrens@gmail.com>
2018-09-27 16:53:39 +0200

    Create array from found nutrition data

--- public/js/renderNutrition.js
+++ public/js/renderNutrition.js
@@ -1,3 +1,22 @@
 async function renderNutrition(dish) {
+  let nutritionData = await $.getJSON('/json/naringsinnehall.json')
   console.log(dish)
+  console.log(nutritionData)
+
+  let nutritionArray = dish.ingredients.map(ingredient => {
+    let data =
+      nutritionData.find(
+        data =>
+          data.Livsmedelsnamn.toLowerCase() === ingredient.name.toLowerCase()
+      ) ||
+      nutritionData.find(data =>
+        data.Livsmedelsnamn.toLowerCase().includes(
+          ingredient.name.toLowerCase()
+        )
+      ) ||
+      null
+      return data
+  }).filter(data => !!data)
+  console.log(nutritionArray)
+  
 }
