6f326d44e9a9a2395d522c9a593eb681546280b1
Dacrol <jonas.ahrens@gmail.com>
2018-09-27 17:36:10 +0200

    Finish up calculations

--- public/js/renderNutrition.js
+++ public/js/renderNutrition.js
@@ -1,22 +1,45 @@
 async function renderNutrition(dish) {
   let nutritionData = await $.getJSON('/json/naringsinnehall.json')
-  console.log(dish)
-  console.log(nutritionData)
+  // console.log(dish)
+  // console.log(nutritionData)
 
-  let nutritionArray = dish.ingredients.map(ingredient => {
-    let data =
-      nutritionData.find(
-        data =>
-          data.Livsmedelsnamn.toLowerCase() === ingredient.name.toLowerCase()
-      ) ||
-      nutritionData.find(data =>
-        data.Livsmedelsnamn.toLowerCase().includes(
-          ingredient.name.toLowerCase()
+  let nutritionArray = dish.ingredients
+    .map(ingredient => {
+      let data =
+        nutritionData.find(
+          data =>
+            data.Livsmedelsnamn.toLowerCase() === ingredient.name.toLowerCase()
+        ) ||
+        nutritionData.find(data =>
+          data.Livsmedelsnamn.toLowerCase().includes(
+            ingredient.name.toLowerCase()
+          )
         )
-      ) ||
-      null
-      return data
-  }).filter(data => !!data)
-  console.log(nutritionArray)
-  
+      return { data: data, mass: ingredient.mass }
+    })
+    .filter(entry => !!entry.data)
+  // console.log(nutritionArray)
+  let nutritionSum = nutritionArray
+    .map(entry => entry.data)
+    .reduce((acc, current, index) => {
+      for (const property in current) {
+        if (current.hasOwnProperty(property)) {
+          if (!isNaN(acc[property])) {
+            if (index === 0) {
+              acc[property] = acc[property] * (nutritionArray[index].mass / 100)
+            }
+            acc[property] =
+              '' +
+              (+acc[property] +
+                +current[property] * (nutritionArray[index].mass / 100))
+          } else delete acc[property]
+        }
+      }
+      return acc
+    })
+  delete nutritionSum.Livsmedelsnummer
+  Object.keys(nutritionSum).map(key => {
+    nutritionSum[key] = Math.round(nutritionSum[key] / dish.portions)
+  })
+  // console.log(nutritionSum)
 }
